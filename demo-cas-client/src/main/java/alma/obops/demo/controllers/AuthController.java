83b5a9fa438b27c808ee56a3fe483c39c0de1574
Maurizio Chavan <amchavan@eso.org>
2018-12-07 16:17:53 +0100

    Refactored (2nd try)

--- demo-cas-client/src/main/java/alma/obops/demo/controllers/AuthController.java
+++ demo-cas-client/src/main/java/alma/obops/demo/controllers/AuthController.java
@@ -1,4 +1,4 @@
-package com.baeldung.cassecuredapp.controllers;
+package alma.obops.demo.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- demo-cas-client/src/main/java/alma/obops/demo/controllers/IndexController.java
+++ demo-cas-client/src/main/java/alma/obops/demo/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.baeldung.cassecuredapp.controllers;
+package alma.obops.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
