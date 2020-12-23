c5ba4952de7c15447304beb0c08cd7b73570d9b3
amchavan <amchavan>
2020-12-23 19:44:45 +0100

    Made API public

--- oidc-resource-server/src/main/java/alma/obops/test/oidcresourceserver/OAuth2ResourceServerController.java
+++ oidc-resource-server/src/main/java/alma/obops/test/oidcresourceserver/OAuth2ResourceServerController.java
@@ -1,10 +1,9 @@
 package alma.obops.test.oidcresourceserver;
 
-
+import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,25 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OAuth2ResourceServerController {
 
-	@GetMapping("/")
-	public Object index(@AuthenticationPrincipal Jwt jwt) {
-		return new Message( String.format( "Hello, %s!", jwt.getClaims().get( "given_name" )));
-	}
-}
-
-class Message {
-	private final String id = UUID.randomUUID().toString();
-	private final String content;
-
-	public Message(String content) {
-		this.content = content;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
+	@GetMapping("/now")
+	public Object index() {
+		var now = new Date();
+		return Map.of( "now", now.toString() );
 	}
 }
